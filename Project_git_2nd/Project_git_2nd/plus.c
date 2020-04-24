dee5aac84746f5f0a5d9b664fedfe5f8d2f8725e
SojeongM <soj0227@naver.com>
2020-04-24 16:20:24 +0900

    visual git 2nd prac

---
+++ Project_git_2nd/Project_git_2nd/plus.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(void) {
+	int a, b, c;
+	printf("1st input : ");
+	scanf("%d", &a);
+	printf("2nd input : ");
+	scanf("%d", &b);
+	c = a + b;
+	printf("1st input + 2nd input = %d\n", c);
+}
